10a51443e1c9bb91660bf93a934ef7a2a8d41486
rafaian <rafaelserafimx@gmail.com>
2026-04-21 14:28:37 -0300

    Adiciona gitignore e corrige estrutura do repositório

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,6 @@
 const express = require("express")
-const cors = require("cors")
-
 const app = express()
-
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
 app.use(express.json())
-
 const PORT = process.env.PORT || 3000
 const { criarBanco } = require("./database")
 
